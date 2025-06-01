d8304d31e6d358896b5c267acab69829434f888b
Fleynqq <63480315+hydraqq@users.noreply.github.com>
2025-06-01 20:16:59 +0300

    Delete Компьютерные науки(Java)/final2 directory

--- Компьютерные науки(Java)/final2/src/main/java/com/example/bookmanager/model/Book.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookmanager.model;
-
-public interface Book {
-    Long getId();
-    void setId(Long id);
-    String getTitle();
-    String getCategory();
-    String getDescription();
-}
