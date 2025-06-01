8181f896d60c4eff5f2e9a1569461e883568260e
Fleynqq <63480315+hydraqq@users.noreply.github.com>
2025-06-01 20:12:00 +0300

    Add files via upload

---
+++ Компьютерные науки(Java)/final2/final2/src/test/java/com/example/bookmanager/BookManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BookManagerApplication.class)
+class BookManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
