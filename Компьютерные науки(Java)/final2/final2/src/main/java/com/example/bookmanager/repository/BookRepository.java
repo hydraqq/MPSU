c39cdcf1b56c7f82a58fed4e3b9f31598b074fc3
Fleynqq <63480315+hydraqq@users.noreply.github.com>
2025-06-01 20:16:34 +0300

    Delete Компьютерные науки(Java)/final2/final2 directory

--- Компьютерные науки(Java)/final2/final2/src/main/java/com/example/bookmanager/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookmanager.repository;
-
-import com.example.bookmanager.model.AbstractBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<AbstractBook, Long> {
-}
